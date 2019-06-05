9bd41e4584eaff15fda6536ff805bc81f86edbd0
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-06-05 20:50:02 +0300

    add a benchmark

---
+++ examples/bench.rs
@@ -0,0 +1,22 @@
+use once_cell::sync::OnceCell;
+
+const N_THREADS: usize = 32;
+const N_ROUNDS: usize = 100_000_000;
+
+static CELL: OnceCell<usize> = OnceCell::new();
+
+fn main() {
+    let threads = (0..N_THREADS)
+        .map(|i| std::thread::spawn(move || thread_main(i)))
+        .collect::<Vec<_>>();
+    for thread in threads {
+        thread.join().unwrap();
+    }
+}
+
+fn thread_main(i: usize) {
+    for _ in 0..N_ROUNDS {
+        let &value = CELL.get_or_init(|| i);
+        assert!(value < N_THREADS)
+    }
+}
