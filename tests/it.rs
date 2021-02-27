c4f7699124afeb51a4b73006a4328eff8f376b19
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-02-27 19:03:50 +0300

    .

--- tests/it.rs
+++ tests/it.rs
@@ -1,3 +1,54 @@
+#[test]
+fn u_dead() {
+    use std::{
+        sync::{Arc, RwLock},
+        thread,
+        time::Duration,
+    };
+
+    let lock = Arc::new(RwLock::new(()));
+
+    let r1 = thread::spawn({
+        let lock = Arc::clone(&lock);
+        move || {
+            let _rg = lock.read();
+            eprintln!("r1/1");
+            sleep(1000);
+
+            let _rg = lock.read();
+            eprintln!("r1/2");
+
+            sleep(5000);
+        }
+    });
+    sleep(100);
+    let w = thread::spawn({
+        let lock = Arc::clone(&lock);
+        move || {
+            let _wg = lock.write();
+            eprintln!("w");
+        }
+    });
+    sleep(100);
+    let r2 = thread::spawn({
+        let lock = Arc::clone(&lock);
+        move || {
+            let _rg = lock.read();
+            eprintln!("r2");
+            sleep(2000);
+        }
+    });
+
+    r1.join().unwrap();
+    r2.join().unwrap();
+    w.join().unwrap();
+}
+
+fn sleep(ms: u64) {
+    std::thread::sleep(Duration::from_millis(ms))
+}
+
+
 mod unsync {
     use core::{
         cell::Cell,
