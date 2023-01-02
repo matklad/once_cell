cc07949250ed44b208ff3d19af8f6b6577fea1e2
Martin Nordholts <enselic@gmail.com>
2023-01-02 16:49:25 +0100

    Explain safety of `unsync::OnceCell::get(&self)` in more detail

--- src/lib.rs
+++ src/lib.rs
@@ -454,7 +454,10 @@ pub mod unsync {
         /// Returns `None` if the cell is empty.
         #[inline]
         pub fn get(&self) -> Option<&T> {
-            // Safe due to `inner`'s invariant
+            // Safe due to `inner`'s invariant of being written to at most once.
+            // Had multiple writes to `inner` been allowed, a reference to the
+            // value we return now would become dangling by a write of a
+            // different value later.
             unsafe { &*self.inner.get() }.as_ref()
         }
 
